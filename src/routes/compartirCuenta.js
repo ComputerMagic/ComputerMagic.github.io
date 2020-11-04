d62f7b44b01e8766495c14e368c457e383f4d3c7
inosyke <bogdan.pogorielow@gmail.com>
2020-11-04 07:32:04 +0200

    master

--- src/routes/compartirCuenta.js
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const listaCompartir = writable(0);
-
-export const totalCompartir = writable(0);
-
-export const divisionCompartir = writable(0);
-
-export const cantidadDePersonasCompartir = writable(0);
