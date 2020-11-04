0232df40265e17707b69e8d28275bae24bc6d23b
inosyke <bogdan.pogorielow@gmail.com>
2020-11-04 09:08:09 +0200

    Add files via upload

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+	import { isLoading, waitLocale } from 'svelte-i18n';
+
+  	export async function preload(page) {
+    	return waitLocale();
+  	}
+</script>
+
+<script>
+	import Header from '../components/Header.svelte';
+	import Footer from '../components/Footer.svelte';
+</script>
+
+{#if $isLoading}
+  	<div class="loading">Loading...</div>
+{:else}
+<div id="app">
+	<Header />
+
+	<div class="container">
+		<slot></slot>
+	</div>
+
+	<Footer />
+</div>
+{/if}
